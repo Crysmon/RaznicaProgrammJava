14ebdfaed0a2992ec4e31cc4ca84eb5d6a23ef11
Crysmon <crysmon-play@mail.ru>
2022-12-15 23:40:58 +0300

    Final Version

---
+++ GraphicsAndSpace/src/Functions.java
@@ -0,0 +1,14 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Functions {
+
+}
